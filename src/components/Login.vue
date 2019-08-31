e95739e2a705f6dd69f3aa4664807c880d2ada22
msimsek <murat.simsek@tav.aero>
2019-08-31 20:30:23 +0300

    develop
    (cherry picked from commit 60bd372c4ee6c9149f938e0f369ac9aaf7b9dc05)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,24 +8,24 @@
 <!--        <a href="#" class="btn btn-info btn-block"><i class="fa fa-twitter"></i> Sign in with <b>Twitter</b></a>-->
 <!--        <a href="#" class="btn btn-danger btn-block"><i class="fa fa-google"></i> Sign in with <b>Google</b></a>-->
 <!--      </div>-->
-      <div class="or-seperator"><i>or</i></div>
+<!--      <div class="or-seperator"><i>or</i></div>-->
       <div class="form-group">
         <div class="input-group">
           <span class="input-group-addon"><i class="fa fa-user"></i></span>
-          <input type="text" class="form-control" name="username" placeholder="Username" required="required">
+          <input type="text" class="form-control" name="username" placeholder="Username" required="required" v-model="form.email">
         </div>
       </div>
       <div class="form-group">
         <div class="input-group">
           <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-          <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+          <input type="password" class="form-control" name="password" placeholder="Password" required="required" v-model="form.password">
         </div>
       </div>
       <div class="form-group">
-        <button type="submit" class="btn btn-success btn-block">Sign in</button>
+        <button type="submit" class="btn btn-success btn-block" @click="onSubmit">Sign in</button>
       </div>
       <div class="clearfix">
-        <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+        <label class="pull-left checkbox-inline"><input type="checkbox" v-model="form.remember"> Remember me</label>
         <a href="#" class="pull-right text-success">Forgot Password?</a>
       </div>
 
@@ -45,36 +45,34 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       form: {
         email: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      show: true
+        password: '',
+        remember: false,
+        response: ''
+      }
     }
   },
   methods: {
     onSubmit (evt) {
       evt.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    onReset (evt) {
-      evt.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
+
+      axios
+        .post('http://www.mocky.io/v2/5d6aae773100006400d20744', {
+          email: this.form.email,
+          password: this.form.password
+        })
+        .then(response => (console.log(response)))
+        .catch(error => {
+          console.log('error : ' + error)
+        })
+
+      // console.log(this.form.response)
+      // alert(JSON.stringify(this.form.response))
     }
   }
 }
